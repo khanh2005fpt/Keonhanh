0231bdfcae1e04641a144a24ed4bbe64a345d9d9
khanh2005fpt <nguyenhuubaokhanh2005@gmail.com>
2026-05-18 20:41:57 +0700

    set up backend for project

---
+++ keonhanh-backend/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import cors from "cors"
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(() => {
+    console.log("DB connected successfully");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+        
+    })
+     
+}).catch((error) => console.log(error))
+   
